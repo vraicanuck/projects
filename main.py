55fdc8bdb4f06868c54918e7acdfcbd55d881cca
vraicanuck <drengurinn.i.gardinum@gmail.com>
2021-01-05 01:13:22 +0300

    Venv deleted

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+import pandas as pd
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def print_df(name):
+    print(pd.DataFrame(index = [name], columns=['name']))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Vladislav')
+    print_df('Vladislav_Elesin')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
